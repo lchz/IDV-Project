eb4b68f4a5896bdbf1282039a7bc93b61a210e73
lchz <32270417+lchz@users.noreply.github.com>
2021-04-17 22:14:06 +0300

    choropleth map with counts of universities

--- app.py
+++ app.py
@@ -7,45 +7,63 @@ import plotly.graph_objects as go
 import plotly.express as px
 import geopandas as gpd
 
+import pandas as pd
+
 ## Initiate the app ###
 app = dash.Dash(__name__)
 server = app.server
 ######################
 
+df = pd.read_csv('count_by_country.csv')
+fig = go.Figure(data=go.Choropleth(
+    locationmode='country names',
+    locations = df['Country'],
+    z = df['Count'],
+    text = df['Country'],
+    colorscale= 'YlGnBu',
+    autocolorscale=False,
+    reversescale=False,
+    marker_line_color='darkgray',
+    marker_line_width=0.5,
+    colorbar_title = 'Count',
+))
+
 ## Layout
 app.layout = html.Div([
-    html.P("Color:"),
-    dcc.Dropdown(
-        id="dropdown",
-        options=[
-            {'label': x, 'value': x}
-            for x in ['Gold', 'MediumTurquoise', 'LightGreen']
-        ],
-        value='Gold',
-        clearable=False,
-    ),
-    dcc.Graph(id="graph"),
+    # html.P("Color:"),
+    # dcc.Dropdown(
+    #     id="dropdown",
+    #     options=[
+    #         {'label': x, 'value': x}
+    #         for x in ['Gold', 'MediumTurquoise', 'LightGreen']
+    #     ],
+    #     value='Gold',
+    #     clearable=False,
+    # ),
+    # dcc.Graph(id="graph"),
+    dcc.Graph(figure=fig)
 ])
 
-@app.callback(
-    Output("graph", "figure"), 
-    [Input("dropdown", "value")])
-def display_color(color):
-    # fig = go.Figure(
-        # data=go.Bar(y=[2, 3, 1], marker_color=color))
-
-    geo_df = gpd.read_file(gpd.datasets.get_path('naturalearth_cities'))
-    # geo_df = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
+# @app.callback(Output("graph", "figure"), [Input("dropdown", "value")])
+# def display_color(color):
+#     # fig = go.Figure(
+#         # data=go.Bar(y=[2, 3, 1], marker_color=color))
 
 
-    # px.set_mapbox_access_token(open(".mapbox_token").read())
-    fig = px.scatter_mapbox(geo_df,
-                            lat=geo_df.geometry.y,
-                            lon=geo_df.geometry.x,
-                            hover_name="name",
-                            zoom=1)
-    fig.update_layout(mapbox_style="open-street-map")
-    return fig
+#      ######################################
+
+#     geo_df = gpd.read_file(gpd.datasets.get_path('naturalearth_cities'))
+#     # geo_df = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
+
+
+#     # px.set_mapbox_access_token(open(".mapbox_token").read())
+#     fig = px.scatter_mapbox(geo_df,
+#                             lat=geo_df.geometry.y,
+#                             lon=geo_df.geometry.x,
+#                             hover_name="name",
+#                             zoom=1)
+#     fig.update_layout(mapbox_style="open-street-map")
+#     return fig
 
 
 if __name__ == "__main__":
